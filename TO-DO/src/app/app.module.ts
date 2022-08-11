9b4c948f53a9b6f06c990b64f840881319e634a8
Rocio Mini <rociomini25@gmail.com>
2022-08-11 15:50:53 -0300

    dd

--- TO-DO/src/app/app.module.ts
+++ TO-DO/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.component';
@@ -12,7 +13,8 @@ import { ListasComponent } from './listas/listas.component';
     ListasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- TO-DO/src/app/formulario/formulario.component.ts
+++ TO-DO/src/app/formulario/formulario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Tarea} from '../../models/tarea.models';
+import { Tarea} from '../models/tarea.models';
 
 @Component({
   selector: 'app-formulario',
