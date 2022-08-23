4122441fcd847473ecd129241af05efb145f57a8
Rocio Mini <rociomini25@gmail.com>
2022-08-23 10:50:38 -0300

    0.0.01

--- Rutas/src/app/app-routing.module.ts
+++ Rutas/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './componentes/about/about.component';
+import { ContactComponent } from './componentes/contact/contact.component';
+import { InfoComponent } from './componentes/info/info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',  pathMatch:'full', redirectTo:'informacion'},
+  {path:'informacion', component:InfoComponent},
+  {path: 'about',  redirectTo: 'sobre'},
+  {path: 'sobre', component:AboutComponent},
+  {path: 'contacto', component: ContactComponent},
+  {path: '**',  redirectTo: 'contacto'},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
