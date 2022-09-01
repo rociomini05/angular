f0d32567aab2f601b1c4367830efd7ecba780be3
Rocio Mini <rociomini25@gmail.com>
2022-09-01 11:29:19 -0300

    0.00

---
+++ Formulario/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateComponent } from './template/template.component';
+import { ModelComponent } from './model/model.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateComponent,
+    ModelComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
